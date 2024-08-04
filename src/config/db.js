abadec6d949f5e3f67095deb4d4c2e4a2be9f2d2
21Dipti <117078531+21Dipti@users.noreply.github.com>
2024-08-04 18:57:20 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://vasu:3fsABVnfFT9Ivtls@cluster0.834pgjl.mongodb.net/');
+    await mongoose.connect('mongodb+srv://vasu:3fsABVnfFT9Ivtls@cluster0.834pgjl.mongodb.net/quadiro');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
